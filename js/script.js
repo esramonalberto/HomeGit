b4cc3745a0e58eacbd06e27f7ae1d943d84b335e
Ramon Alberto <esramonalberto@gmail.com>
2017-04-19 02:02:07 -0400

    He bajo del numero

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 document.write("Mi nombre es Ramon Alberto -_- " + "<br />");
-document.write("Este es mi correo 'esramonalberto@gmail.com'");
+document.write("Este es mi correo 'esramonalberto@gmail.com'"+ "<br />");
 document.write("Telefono 809-666-2010");
